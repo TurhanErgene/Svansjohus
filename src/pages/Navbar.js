e996bbc6820733bc7d27ce61c7bafd99714df52a
Turhan <66634671+TurhanErgene@users.noreply.github.com>
2023-05-13 01:32:43 +0200

    adding dropdown to navbar components

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -4,141 +4,149 @@ import { motion } from "framer-motion";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleToggle = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const navLinkVariants = {
-    hidden: { opacity: 0, x: -20 },
-    visible: { opacity: 1, x: 0 },
-  };
+  const navbarItems = [
+    { label: "Home", link: "/" },
+    {
+      label: "Services",
+      link: "/",
+      dropdownItems: [
+        { label: "Web Design", link: "/" },
+        { label: "Graphic Design", link: "/" },
+        { label: "Marketing", link: "/" },
+      ],
+    },
+    { label: "About", link: "/" },
+    { label: "Contact", link: "/" },
+  ];
 
   return (
     <nav className="bg-gray-800">
-      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div className="relative flex items-center justify-between h-16">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <button
-              type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-              aria-controls="mobile-menu"
-              aria-expanded="false"
-              onClick={handleToggle}
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className={`${isOpen ? "hidden" : "block"} h-6 w-6`}
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-              <svg
-                className={`${isOpen ? "block" : "hidden"} h-6 w-6`}
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-          </div>
-          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex-shrink-0">
-              <a href="/" className="text-white font-bold text-lg">
-                My Site
-              </a>
-            </div>
-            <div className="hidden sm:block sm:ml-6">
-              <div className="flex space-x-4">
-                <motion.div
-                  initial="hidden"
-                  animate="visible"
-                  variants={navLinkVariants}
-                  transition={{ duration: 0.5 }}
-                >
-                  <a
-                    href="/"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Home
-                  </a>
-                </motion.div>
-                <motion.div
-                  initial="hidden"
-                  animate="visible"
-                  variants={navLinkVariants}
-                  transition={{ duration: 0.5, delay: 0.1 }}
-                >
-                  <a
-                    href="/"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                    About
-                  </a>
-                </motion.div>
-                <motion.div
-                  initial="hidden"
-                  animate="visible"
-                  variants={navLinkVariants}
-                  transition={{ duration: 0.5, delay: 0.2 }}
-                >
-                  <a
-                    href="/"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Contact
-                  </a>
-                </motion.div>
+      <div className="mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16">
+          <div className="flex items-center">
+            <a href="/" className="flex-shrink-0">
+              <img
+                className="h-8 w-8"
+                src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                alt="Workflow"
+              />
+            </a>
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+                {navbarItems.map((item) => (
+                  <div key={item.label}>
+                    <a
+                      href={item.link}
+                      className="px-3 py-2 rounded-md text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
+                    >
+                      {item.label}
+                    </a>
+                    {item.dropdownItems && (
+                      <div className="hidden absolute mt-1 w-56 rounded-md bg-white shadow-lg">
+                        {item.dropdownItems.map((dropdownItem) => (
+                          <a
+                            key={dropdownItem.label}
+                            href={dropdownItem.link}
+                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                          >
+                            {dropdownItem.label}
+                          </a>
+                        ))}
+                      </div>
+                    )}
+                  </div>
+                ))}
               </div>
             </div>
           </div>
+          <div className="-mr-2 flex md:hidden">
+            {/* Hamburger menu */}
+            <motion.button
+              onClick={() => setIsOpen(!isOpen)}
+              animate={isOpen ? "open" : "closed"}
+              variants={{
+                open: { rotate: 90 },
+                closed: { rotate: 0 },
+              }}
+              transition={{ duration: 0.2 }}
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 transition duration-150 ease-in-out"
+              aria-label="Main menu"
+              aria-expanded="false"
+            >
+              {isOpen ? (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </motion.button>
+          </div>
         </div>
       </div>
-    
-      <div
-        className={`${
-          isOpen ? "block" : "hidden"
-        } sm:hidden`}
-        id="mobile-menu"
+
+      {/* Side menu */}
+      <motion.div
+        initial={{ x: "-100%" }}
+        animate={isOpen ? { x: 0 } : { x: "-100%" }}
+        transition={{ duration: 0.2 }}
+        className="md:hidden"
       >
-        <div className="px-2 pt-2 pb-3 space-y-1">
-          <a
-            href="/"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
-            Home
-          </a>
-          <a
-            href="/"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
-            About
-          </a>
-          <a
-            href="/"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
-            Contact
-          </a>
+        <div className="px-2 pt-2 pb-3 sm:px-3">
+          {navbarItems.map((item) => (
+            <div key={item.label}>
+              <a
+                href={item.link}
+                className="block px-3 py-2 rounded-md text-base font-medium text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
+              >
+                {item.label}
+              </a>
+              {item.dropdownItems && (
+                <div className="ml-3 mt-1">
+                  {item.dropdownItems.map((dropdownItem) => (
+                    <a
+                      key={dropdownItem.label}
+                      href={dropdownItem.link}
+                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+                    >
+                      {dropdownItem.label}
+                    </a>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
         </div>
-      </div>
+      </motion.div>
     </nav>
-      );
-  };
+  );
+};
 
 export default Navbar;
